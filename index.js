145215313d8b1775e2c36b77bb0949ce10b5af02
elwira-m <36117564+elwira-m@users.noreply.github.com>
2018-06-05 00:35:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const formatDate = (timeInSeconds) => {
+    if (!timeInSeconds) {
+        return '0s';
+    }
+
+    const hour = Math.floor((timeInSeconds / 3600));
+    const min = Math.floor((timeInSeconds % 3600) / 60);
+    const sec = Math.floor((timeInSeconds % 60));
+
+    return [
+        hour ? hour + 'h ' : '',
+        min ? min + 'm ' : '',
+        sec ? sec + 's ' : ''
+    ].join('').trim();
+} 
+
+module.exports = formatDate;
