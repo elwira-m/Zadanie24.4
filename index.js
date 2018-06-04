1415c00af459f577b7840c777ab068d26da44ff3
elwira-m <36117564+elwira-m@users.noreply.github.com>
2018-06-05 01:40:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const formatDate = (timeInSeconds) => {
         return '0s';
     }
 
-    const hour = Math.floor((timeInSeconds / 3600));
+    const hour = Math.floor(timeInSeconds / 3600);
     const min = Math.floor((timeInSeconds % 3600) / 60);
-    const sec = Math.floor((timeInSeconds % 60));
+    const sec = Math.floor(timeInSeconds % 60);
 
     return [
         hour ? hour + 'h ' : '',
